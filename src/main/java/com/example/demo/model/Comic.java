26d7681b2181b894f0d1fe7ffef077c546e6f5de
Sang <sangliverpool395@gmail.com>
2024-10-17 09:43:53 +0700

    sua lai api tinh thanh

--- src/main/java/com/example/demo/model/Comic.java
+++ src/main/java/com/example/demo/model/Comic.java
@@ -28,7 +28,6 @@ public class Comic {
     @Id
      @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
     private String name;
     private double price;
     private String url;
